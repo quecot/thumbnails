3557430d76b30dc187bb23ff07ac2177c975d254
Francesc Vila <francescvilasubias@gmail.com>
2022-10-08 09:55:27 +0200

    chore: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.ts',
+    './src/**/*.tsx',
+    './assets/html/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ webpack.config.ts
@@ -0,0 +1,47 @@
+import { Configuration } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+import 'webpack-dev-server';
+
+const configuration: Configuration = {
+  entry: './src/main.tsx',
+  mode: 'development',
+  resolve: {
+    plugins: [
+      new TsconfigPathsPlugin(),
+    ],
+    extensions: [
+      '.ts',
+      '.tsx',
+      '.js',
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.tsx$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './assets/html/index.html',
+    }),
+  ],
+  devServer: {
+    client: {
+      logging: 'warn',
+    },
+  },
+};
+
+export default configuration;
