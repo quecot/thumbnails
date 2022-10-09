ebff0431e648e58aed6e32513fac6ddbbdee5186
Francesc Vila <francescvilasubias@gmail.com>
2022-10-09 20:02:01 +0200

    fix: remove darkmode from span

--- src/App.tsx
+++ src/App.tsx
@@ -187,7 +187,7 @@ const App = () => {
             // eslint-disable-next-line max-len
                   className="w-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
-                <span className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Conserva colors</span>
+                <span className="ml-2 text-sm font-medium text-gray-900">Conserva colors</span>
               </div>
               <div>
                 <input
@@ -197,7 +197,7 @@ const App = () => {
             // eslint-disable-next-line max-len
                   className="w-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
-                <span className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Conserva patró</span>
+                <span className="ml-2 text-sm font-medium text-gray-900">Conserva patró</span>
               </div>
             </div>
           ) : <div />}
