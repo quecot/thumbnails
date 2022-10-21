e6a78c5263e23bd4adae5ddc1578c30455114c1d
Francesc Vila <francescvilasubias@gmail.com>
2022-10-21 18:31:13 +0200

    fix: show caret in text input

--- src/App.tsx
+++ src/App.tsx
@@ -176,7 +176,7 @@ const App = () => {
             type="text"
             id="videoTitle"
           // eslint-disable-next-line max-len
-            className="w-96 caret-transparent bg-gray-50 border text-center border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+            className="w-96 bg-gray-50 border text-center border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
             placeholder="Títol del vídeo"
           />
           <button
